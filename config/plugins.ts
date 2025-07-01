16ac4559ae8503c158d08a201b34ab62484b9f39
Romain Guillot <rguillot1003@gmail.com>
2025-07-01 08:20:04 +0200

    add nodemailer provider plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,19 @@
-export default () => ({});
+export default ({env }) => ({
+    email: {
+        config: {
+          provider: 'nodemailer',
+          providerOptions: {
+            host: env('SMTP_HOST', 'smtp.gmail.com'),
+            port: env.int('SMTP_PORT', 587),
+            auth: {
+              user: env('SMTP_USERNAME'),
+              pass: env('SMTP_PASSWORD'),
+            },
+          },
+          settings: {
+            defaultFrom: env('SMTP_DEFAULT_FROM', 'noreply@tondomaine.com'),
+            defaultReplyTo: env('SMTP_DEFAULT_REPLYTO', 'noreply@tondomaine.com'),
+          },
+        },
+    },
+});
