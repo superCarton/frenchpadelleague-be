bb7038d6ceecf1f4ca39473b10de5c06e15a1f7e
Romain Guillot <rguillot1003@gmail.com>
2025-08-19 10:39:51 +0200

    configure cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -42,5 +42,19 @@ export default ({env }) => ({
             },
             generateDefaultDocumentation: true,
         },
-    }
+    },
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
 });
